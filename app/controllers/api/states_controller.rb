4a5f4d88d86c70362614d73f0b88938a249be7de
austin shields <ajshields196@gmail.com>
2021-06-06 15:22:59 -0400

    render state data as json in states show action

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -1,8 +1,18 @@
 class Api::StatesController < ApplicationController
   def show
-    @states = {
-      message: "oi"
-    }
-    render json: @states
+    @data = []
+
+    CSV.foreach(Rails.root.join('lib/state_data.csv'), skip_lines: /^#/, headers: true) do |row|
+      state = {
+        state: row[0],
+        median_household_income: row[1],
+        share_unemployed_seasonal: row[2],
+        share_population_in_metro_areas: row[3],
+        share_population_with_high_school_degree: row[4]
+      }
+      @data << state
+    end
+
+    render json: @data
   end
 end
