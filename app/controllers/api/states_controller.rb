1197af9621bac848eea8ed935ac8480819d503ef
austin shields <ajshields196@gmail.com>
2021-06-07 21:23:47 -0400

    refactor states index action

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -2,16 +2,11 @@ class Api::StatesController < ApplicationController
   def show
     @data = []
 
-    CSV.foreach(Rails.root.join('lib/state_data.csv'), skip_lines: /^#/, headers: true) do |row|
-      state = {
-        state: row[0],
-        median_household_income: row[1],
-        share_unemployed_seasonal: row[2],
-        share_population_in_metro_areas: row[3],
-        share_population_with_high_school_degree: row[4]
-      }
-      @data << state
+    CSV.foreach(Rails.root.join('lib/state_data.csv'), headers: true, converters: :all) do |row|
+      @data << row.to_hash
     end
+
+    @data = @data.to_json
 
     render json: @data
   end
