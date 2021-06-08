a13908d9360059066de28f00a83098e2b5aa45f5
austin shields <ajshields196@gmail.com>
2021-06-07 21:51:01 -0400

    convert abbr to state name in states show action

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -1,4 +1,5 @@
 class Api::StatesController < ApplicationController
+  #/api/states
   def index
     @data = []
 
@@ -11,9 +12,18 @@ class Api::StatesController < ApplicationController
     render json: @data
   end
 
+  #/api/states/:abbr
   def show
-    data = [params[:abbr]]
+    state = false
+    #convert abbr to state name
+    CSV.foreach(Rails.root.join('lib/abbreviations.csv'), headers: true, converters: :all) do |row|
+      hash = row.to_hash
+      if hash["Code"] == params[:abbr]
+        state = hash["State"]
+        break
+      end
+    end
 
-    render json: data
+    render json: state
   end
 end
